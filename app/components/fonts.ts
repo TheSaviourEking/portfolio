9479756c942b6f660c54f61a885da232bdeb5241
TheSaviourEking <saviour.eking@outlook.com>
2024-07-22 00:21:13 +0100

    chore: fix PostCard components

--- app/components/fonts.ts
+++ app/components/fonts.ts
@@ -1,5 +1,6 @@
-import { Inter } from 'next/font/google';
+import { Heebo, Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
+export const heebo = Heebo({ weight: ["100", "200", "300", "400", "500", "600", "700"], subsets: ['latin'] });
