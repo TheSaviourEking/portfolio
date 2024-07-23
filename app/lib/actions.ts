4c4c9ff3de8325eebed784e4c27ed125359d865a
TheSaviourEking <saviour.eking@outlook.com>
2024-07-23 04:15:16 +0100

    chore: update actions, data and utils

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,6 @@
-import { posts, projects } from "./data"
+import { linkedInArticles, projects } from "./data";
+import Parser from 'rss-parser';
+
 // import { PrismaClient } from '@prisma/client'
 
 // const prisma = new PrismaClient()
@@ -37,4 +39,35 @@ export async function fetchRecentProjects() {
 
 export async function fetchAllPosts() {
     return posts;
+}
+
+
+const parser = new Parser();
+
+async function fetchHashnodeArticles() {
+    const feed = await parser.parseURL('https://thesavioureking.hashnode.dev/rss.xml');
+    console.log(feed, '\n----------------------------');
+    return feed.items.map(item => ({
+        title: item.title,
+        link: item.link,
+        pubDate: item.pubDate,
+        content: item.contentSnippet,
+        categories: item.categories,
+        source: 'Hashnode'
+    }));
+}
+
+export async function articleHandler() {
+    try {
+        const hashnodeArticles = await fetchHashnodeArticles();
+        const allArticles = [...hashnodeArticles, ...linkedInArticles];
+
+        // Sort articles by date, most recent first
+        allArticles.sort((a, b) => new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime());
+
+        return allArticles;
+    } catch (error) {
+        console.log(error, 'ERROR')
+        // res.status(500).json({ error: 'Failed to fetch RSS feed' });
+    }
 }
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,16 +1,22 @@
-export const formateDate = (dateStr: Date): String => {
-    // const date = new Date('2020-02-12');
-    const options = { day: '2-digit', month: 'short', year: 'numeric' };
-    const formattedDate = dateStr.toLocaleDateString('en-US', options);
-    // console.log(formattedDate); // "12 Feb 2020"
-    return formattedDate;
+// export const formateDate = (dateStr: Date): String => {
+//     // const date = new Date('2020-02-12');
+//     const options = { day: '2-digit', month: 'short', year: 'numeric' };
+//     const formattedDate = dateStr.toLocaleDateString('en-US', options);
+//     // console.log(formattedDate); // "12 Feb 2020"
+//     return formattedDate;
 
-    // const date = new Date(dateStr);
-    // const options: Intl.DateTimeFormatOptions = {
-    //     day: 'numeric',
-    //     month: 'short',
-    //     year: 'numeric',
-    // };
-    // const formatter = new Intl.DateTimeFormat('locale', options);
-    // return formatter.format(date);
-}
+//     // const date = new Date(dateStr);
+//     // const options: Intl.DateTimeFormatOptions = {
+//     //     day: 'numeric',
+//     //     month: 'short',
+//     //     year: 'numeric',
+//     // };
+//     // const formatter = new Intl.DateTimeFormat('locale', options);
+//     // return formatter.format(date);
+// }
+
+export function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { day: '2-digit', month: 'short', year: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  }
