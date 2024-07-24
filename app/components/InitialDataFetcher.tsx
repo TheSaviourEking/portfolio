3f94fccba058ae75d9a184325d72b0a43cfbf963
TheSaviourEking <saviour.eking@outlook.com>
2024-07-24 15:57:59 +0100

    chore: remove redux store
    Had issues managing server side state with Redux, had to create my own global store on the server to manage state

--- app/components/InitialDataFetcher.tsx
+++
@@ -1,9 +0,0 @@
-// app/components/InitialDataFetcher.jsx
-'use client';
-
-import { useInitialFetch } from '../hooks/useInitialFetch';
-
-export default function InitialDataFetcher({ children }) {
-    useInitialFetch();
-    return <>{children}</>;
-}
