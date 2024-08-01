a1fb107d8d0fd209e3b553491485753749767821
TheSaviourEking <saviour.eking@outlook.com>
2024-08-01 10:20:13 +0100

    chore: Update the copyright page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Metadata } from 'next';
 import { useState, ChangeEvent, FormEvent } from 'react';
 
 export default function ContactPage() {
@@ -40,62 +39,93 @@ export default function ContactPage() {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Contact Me</h1>
-      {submitted ? (
-        <p className="text-green-500">Thank you for your message!</p>
-      ) : (
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label htmlFor="name" className="block text-sm font-medium">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
-              required
-            />
+    <div className='flex flex-col md:flex-row'>
+      <div className='basis-1/2 p-4 b'>
+        <h2 className="text-2xl font-bold mb-4">Get in Touch</h2>
+        <p className="mb-4">
+          I&apos;m always excited to connect for:
+        </p>
+        <ul className="list-disc list-inside mb-4 space-y-1">
+          <li>Business inquiries</li>
+          <li>Collaboration opportunities</li>
+          <li>Speaking engagements</li>
+          <li>General questions</li>
+        </ul>
+        <p className="mb-4">
+          Use the form to reach out. I&apos;ll respond within 48 hours.
+        </p>
+        <div className="mt-6">
+          <h3 className="text-xl font-semibold mb-2">Connect with me:</h3>
+          <div className="flex flex-wrap gap-4">
+            <a href="https://www.linkedin.com/in/saviour-eking" target="_blank" rel="noopener noreferrer" className='contact-me-options'>
+              {/* <FaLinkedin className='socials' /> */} LinkedIn
+            </a>
+            <a href="https://twitter.com/TheSaviourEking" target="_blank" rel="noopener noreferrer" className='contact-me-options'>
+              {/* <FaTwitter className='socials' /> */} Twitter
+            </a>
+            <a href="https://github.com/TheSaviourEking" target="_blank" rel="noopener noreferrer" className='contact-me-options'>
+              {/* <FaGithub className='socials' /> */} Github
+            </a>
           </div>
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="message" className="block text-sm font-medium">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
-              rows={4}
-              required
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-          >
-            Submit
-          </button>
-        </form>
-      )}
+        </div>
+      </div>
+      <div className="basis-1/2 container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-4">Contact Me</h1>
+        {submitted ? (
+          <p className="text-green-500">Thank you for your message!</p>
+        ) : (
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="message" className="block text-sm font-medium">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                rows={4}
+                required
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
