c5ba39033f0d7a1c94046ea315e8bb5ed853912f
TheSaviourEking <saviour.eking@outlook.com>
2024-12-18 12:43:24 -0700

    chore: add local fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,12 +2,12 @@ import { GFS_Neohellenic } from "next/font/google"
 
 export const gfsNeohellenic = GFS_Neohellenic(
     {
-      weight: ['400', '700'],
-      style: ['normal', 'italic'],
-      display: 'swap',
-      subsets: ['greek'],
-      variable: '--font-neohellenic',
-      preload:true,
-      
+        weight: ['400', '700'],
+        style: ['normal', 'italic'],
+        display: 'swap',
+        subsets: ['greek'],
+        variable: '--font-neohellenic',
+        //   preload:true,
+
     }
-  )
+)
