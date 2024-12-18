920de7f21eeffddb4e66838d904021934975f740
TheSaviourEking <saviour.eking@outlook.com>
2024-12-18 12:41:56 -0700

    chore: add linkedin articles

---
+++ src/lib/data/store.ts
@@ -0,0 +1,37 @@
+export const linkedInArticles = [
+    {
+        title: 'Required Skills As A Software Engineer',
+        link: 'https://www.linkedin.com/pulse/required-skills-software-engineer-saviour-eking/?trackingId=kwOyaCs%2FQWu3eC2BDJ%2FUyw%3D%3D',
+        pubDate: '2022-10-3',//October 3, 2022
+        content: 'Every wannabe Software engineer has had to ask this question "What are the required skills to be a proficient Software Engineer?". I have asked myself, teachers and friends more than enough times, unfortunately, their answers were never convincing enough.',
+        categories: ['software engineering', 'programming']
+    },
+    {
+        title: 'What is Software Engineering',
+        link: 'https://www.linkedin.com/pulse/what-software-engineering-saviour-eking/?trackingId=kwOyaCs%2FQWu3eC2BDJ%2FUyw%3D%3D',
+        pubDate: '2022-10-6',//October 6, 2022
+        content: 'You have probably seen that dude(him/her/they) who is usually with his laptop, and seems to be working always, you then approached him and inquired what he does, then he said "I am a Software Engineer", You wandered what that means because you have always admired his charisma and always wanted to be just like him. Well, in this article, we will be discussing software engineering and why you should be one.',
+        categories: ['software engineering', 'programming']
+    },
+    {
+        title: 'For Loops Vs While Loops',
+        link: 'https://www.linkedin.com/pulse/loops-vs-while-saviour-eking/?trackingId=kwOyaCs%2FQWu3eC2BDJ%2FUyw%3D%3D',
+        pubDate: '2022-11-25',//November 25, 2022
+        content: 'Loops are among the most basic and powerful of programming concepts. A loop in a computer program is an instruction that repeats until a specified condition is reached. In a loop structure, the loop asks a question. If the answer requires action, it is executed. The same question is asked again and again until no further action is required. Each time the question is asked is called an iteration. ',
+        categories: ['software engineering', 'programming']
+    },
+    {
+        title: 'What is Valgrind',
+        link: 'https://www.linkedin.com/pulse/what-valgrind-saviour-eking/?trackingId=kwOyaCs%2FQWu3eC2BDJ%2FUyw%3D%3D',
+        pubDate: '2022-12-13',//December 13, 2022
+        content: "As a software developer working with C or C++, chances are you've heard of Valgrind. This popular tool is used by many developers to detect and debug memory-related errors in their programs.",
+        categories: ['alx', 'software engineering', 'computer programming', 'low level learning']
+    },
+    {
+        title: 'Transform Your Terminal: A Step-by-Step Guide to Show Git Branch in Bash Prompt',
+        link: 'https://www.linkedin.com/pulse/transform-your-terminal-step-by-step-guide-show-git-branch-eking/?trackingId=kwOyaCs%2FQWu3eC2BDJ%2FUyw%3D%3D',
+        pubDate: '2023-02-8',//February 8, 2023
+        content: "As a software developer, you likely spend significant time in the terminal. The terminal is indispensable whether you're working with Git, running scripts, or just navigating your file system. But did you know that you can customize your terminal to make your workflow even more efficient?.",
+        categories: ['alx', 'software engineering', 'programming', 'CLI', 'bash', 'linux']
+    },
+];
