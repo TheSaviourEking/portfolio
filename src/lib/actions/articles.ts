48182ec5019cc3de424edce74f5708f06b1d01c8
TheSaviourEking <saviour.eking@outlook.com>
2024-12-18 12:41:33 -0700

    chore: add article fetch logic

---
+++ src/lib/actions/articles.ts
@@ -0,0 +1,38 @@
+'use server'
+
+import Parser from 'rss-parser';
+import { linkedInArticles } from '../data/store';
+
+const parser = new Parser();
+
+async function fetchHashnodeArticles() {
+    const feed = await parser.parseURL('https://thesavioureking.hashnode.dev/rss.xml');
+    return feed.items.map(item => ({
+        title: item.title,
+        link: item.link,
+        pubDate: item.pubDate,
+        content: item.contentSnippet,
+        categories: item.categories,
+        source: 'Hashnode'
+    }));
+}
+
+export async function getArticles() {
+    try {
+        const hashnodeArticles = await fetchHashnodeArticles();
+        const allArticles = [...hashnodeArticles, ...linkedInArticles];
+
+        // Sort articles by date, most recent first
+        allArticles.sort((a, b) => {
+            const dateA = new Date(a.pubDate ?? 0).getTime(); // Default to 0 if undefined
+            const dateB = new Date(b.pubDate ?? 0).getTime(); // Default to 0 if undefined
+            return dateB - dateA;
+        });
+
+
+        return allArticles;
+    } catch (error) {
+        console.log({ error: 'Failed to fetch RSS feed' })
+        return [];
+    }
+}
