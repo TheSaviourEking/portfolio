3c641cd4bf656e183327c54e83021781d833c7eb
TheSaviourEking <saviour.eking@outlook.com>
2024-12-18 12:42:44 -0700

    setup: update allowed image routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true, // Enable SVG rendering
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'opengraph.githubassets.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'link-to-screenshot.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.netlify.com',
+        pathname: '/api/v1/badges/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
